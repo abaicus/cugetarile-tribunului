1bd42d7026f0d983cce1c26ef4f0b426440df6ad
abaicus <baicusandrei@me.com>
2020-04-23 00:06:30 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,11 @@
 
 mkdir dist
 cd dist
-git init
-git remote add origin https://github.com/abaicus/cugetarile-tribunului
-git fetch
-git checkout gh-pages
-cd -
-echo 'after cd root'
-pwd
+git init && git remote add origin https://github.com/abaicus/cugetarile-tribunului
+git fetch && git checkout gh-pages
+cd ..
 npm run build
-
 cd dist
-echo 'after cd dist'
-pwd
-git commit -am "Deploy"
+git commit -am "Deploy" && git remote -v
 git push origin gh-pages
-cd -
+cd ..
