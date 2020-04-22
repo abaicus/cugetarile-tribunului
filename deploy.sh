73da507217d49c3b43fb275e21c4a27da30d261a
abaicus <baicusandrei@me.com>
2020-04-23 00:02:36 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env sh
 
-# abort on errors
-set -e
-
-# build
+mkdir dist
+cd dist
+git init
+git remote add origin https://github.com/abaicus/cugetarile-tribunului
+git fetch
+git checkout gh-pages
+cd -
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+git commit -am "Deploy"
+git push origin gh-pages
 cd -
